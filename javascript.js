624d09737106480673a7e03848d7a711f52ca629
Ryan <43714844+Xiaowei1102@users.noreply.github.com>
2023-07-29 21:11:36 -0500

    display the button clicked(only numbers)

--- javascript.js
+++ javascript.js
@@ -20,3 +20,23 @@ function operate(operator, a, b) {
 
     }
 }
+//create 9 number buttons
+for (let i = 0; i <= 9; i++) {
+    numberButton = document.createElement('button');
+    numberButton.textContent = i;
+    numberButton.classList.add('number')
+    document.body.appendChild(numberButton);
+}
+
+//dispay the clicks on the screen;
+//according to the problem description, only want to display number
+
+
+const clicks = document.querySelectorAll('button');
+clicks.forEach(click => click.addEventListener('click', (e) => {
+    const clickContent = e.target.textContent;
+    if (clickContent >= '0' && clickContent <= '9') {
+        const display = document.querySelector('div');
+        display.textContent = e.target.textContent;
+    }
+}));
