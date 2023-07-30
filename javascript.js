a9c60b0e9feaeb02bd4ded825f36bfc640ee479f
Ryan <43714844+Xiaowei1102@users.noreply.github.com>
2023-07-30 00:51:14 -0500

    store two nums in arr and use previous operator to caculate once current operator is called

--- javascript.js
+++ javascript.js
@@ -31,12 +31,49 @@ for (let i = 0; i <= 9; i++) {
 //dispay the clicks on the screen;
 //according to the problem description, only want to display number
 
+//need some variables to store previous click and previous calculation result
+//use this click to add up the number
+let preClickedNum = '';
+const arr = [null, null];
+//const opArr = [null, null];
+const operators = ['+', '-', '*', '/', '='];
+let currOperator = null;
 
 const clicks = document.querySelectorAll('button');
 clicks.forEach(click => click.addEventListener('click', (e) => {
     const clickContent = e.target.textContent;
+    //display number on the screen
+    const display = document.querySelector('div');
     if (clickContent >= '0' && clickContent <= '9') {
-        const display = document.querySelector('div');
-        display.textContent = e.target.textContent;
+        
+        display.textContent = preClickedNum + clickContent;
+        preClickedNum = display.textContent;
     }
+    //when hitting operator, need to add operator
+    if (operators.includes(clickContent)) {
+        let inputNum = parseInt(preClickedNum, 10)
+        //first number should come from either first time entry
+        //or from calculation
+        if (arr[0] === null) { //initialize the first number
+            arr[0] = inputNum;
+        } else {//otherwise, this entered number must be second number
+            arr[1] = inputNum;
+        }
+        
+        //if this is the first time we enter operator, store in for later use
+        if (currOperator === null) {
+            currOperator = clickContent;
+        } else {//if this is a second operator, we need calculate
+            arr[0] = operate(currOperator, arr[0], arr[1]);
+            display.textContent = arr[0];
+            currOperator = clickContent;
+        }
+        //reset the number adding click
+        preClickedNum = '';
+        
+    }
+    
+    console.log(arr[0]);
+    
+
 }));
